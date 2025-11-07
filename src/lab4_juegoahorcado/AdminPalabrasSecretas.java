3155bdbc156d6cbd142a97da680a8b31cd08ec2c
carmengabrie7 <carmenelisa.gabrie7@gmail.com>
2025-11-07 09:30:55 -0600

    ajustando

--- src/lab4_juegoahorcado/AdminPalabrasSecretas.java
+++ src/lab4_juegoahorcado/AdminPalabrasSecretas.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab4_juegoahorcado;
 
-/**
- *
- * @author DELL
- */
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class AdminPalabrasSecretas {
+    //Gestiona dinamicamente las palabras del juego
+    ArrayList<String> palabrasDisponibles = new ArrayList<String>();
     
+    public AdminPalabrasSecretas (ArrayList<String> palabrasDisponibles){
+        this.palabrasDisponibles=palabrasDisponibles;
+    }
+    
+    public boolean agregarPalabras (String palabra){
+        palabrasDisponibles.add(palabra);
+        return true;
+    }
+    
+    public String obtenerRandom (){
+        Random random = new Random();
+        int indice = random.nextInt(palabrasDisponibles.size());
+        return palabrasDisponibles.get(indice);
+    }
+    
+    public void mostrarPalabras(){
+        System.out.println("Palabras Disponibles");
+        for (String p : palabrasDisponibles){
+            System.out.println("- "+p);
+        }
+    }
 }
--- src/lab4_juegoahorcado/JuegoAhorcadoFijo.java
+++ src/lab4_juegoahorcado/JuegoAhorcadoFijo.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab4_juegoahorcado;
 
-/**
- *
- * @author DELL
- */
-public class JuegoAhorcadoFijo {
+public class JuegoAhorcadoFijo extends JuegoAhorcadoBase {
+
+@Override
+public void actualizarPalabraActual(char letra){
+    
+}
+
+
     
 }
