d22d743393110e2a0cb3e9cf6ce7e81b85a6af4b
Emartorell07 <eduardomartorell38@gmail.com>
2025-11-07 08:53:44 -0600

    clase abstracta

---
+++ src/lab4_juegoahorcado/JuegoAhorcadoBase.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4_juegoahorcado;
+
+/**
+ *
+ * @author eduar
+ */
+import java.util.ArrayList;
+
+public abstract class JuegoAhorcadoBase {
+    
+    protected String palabraSecreta;
+    protected String palabraActual;
+    protected int intentos;
+    protected int limiteIntentos=6;
+    protected ArrayList<Character> letrasUsadas;
+    protected ArrayList<String> figuraAhorcado;
+
+    public JuegoAhorcadoBase() {
+        this.intentos= limiteIntentos;
+        this.letrasUsadas= new ArrayList<>();
+        this.figuraAhorcado= new ArrayList<>();
+        inicializarFigura();    
+    }
+    
+    protected void inicializarFigura(){
+        
+    }
+    
+    
+    
+}
